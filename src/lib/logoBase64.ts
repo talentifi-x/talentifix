75434a078316d135faea346e7c7f6d16203cc870
Diwakar Jha <diwakarjha554@gmail.com>
2026-02-24 16:33:28 +0530

    fix: added logo in mail response, correct tx logo in 2 lines

--- src/lib/logoBase64.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export function getLogoBase64(): string {
-  const logoPath = path.join(process.cwd(), "public", "logos", "logo.png");
-  const logoBuffer = fs.readFileSync(logoPath);
-  return `data:image/png;base64,${logoBuffer.toString("base64")}`;
-}
