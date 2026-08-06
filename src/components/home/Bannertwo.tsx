2731d8aa0c636988473eb15e2493c37385ece7e9
Karanjoshi128 <joshikaran.aad.0007@gmail.com>
2026-08-06 19:44:18 +0530

    Added banner to homepage 2

--- src/components/home/Bannertwo.tsx
+++ src/components/home/Bannertwo.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 export const Bannertwo = () => {
   return (
-    <section className="w-full py-12 bg-[#F7F9FC]">
-      <Image
-        src="/banner/ISO Strip - Transparent.png"
-        alt="ISO Certifications"
-        width={1920}
-        height={250}
-        priority
-        className="w-full h-auto"
-      />
+    <section className="w-full bg-white">
+      <div className="w-full max-w-7xl mx-auto px-6 md:px-8">
+        <Image
+          src="/banner/ISO Strip - Transparent.png"
+          alt="ISO Certifications"
+          width={1920}
+          height={250}
+          priority
+          className="w-full h-auto"
+        />
+      </div>
     </section>
   );
 };
